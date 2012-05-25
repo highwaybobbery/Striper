9646fdaf2cb91114e2acfd56a0deace776e6a08d
Alex Berry <highwaybobbery@gmail.com>
2012-05-25 11:11:22 -0400

    Initial Commit

---
+++ Gemfile
@@ -0,0 +1,24 @@
+source 'https://rubygems.org'
+
+gem 'jquery-rails'
+gem 'mysql2'
+gem 'rails', '3.2.3'
+
+group :assets do
+  gem 'coffee-rails', '~> 3.2.1'
+  gem 'haml'
+  gem 'sass-rails',   '~> 3.2.3'
+  gem 'uglifier', '>= 1.0.3'
+end
+
+group :development, :test do
+  gem 'cucumber-rails', require:false
+  gem 'pry'
+  gem 'rspec-rails'
+end
+
+group :test do
+  gem 'database_cleaner'
+  gem 'factory_girl_rails'
+  gem 'shoulda-matchers'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Striper::Application.config.secret_token = 'd2f3966eb573cfa7c039532bb1b95294baa18089651594ce537209f5a472f7e4735c36721cf0d8a585ef04706ea7fb243e06bf0f361e6bab495a751782ddf780'
